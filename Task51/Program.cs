dbe3a45af29b3b5805ff62088964447615bf853f
Mikhail <mvnorkin@gmail.com>
2022-08-03 22:03:07 +0300

    Task51 is comlite

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int[] array = new int [4];
+int a = 0;
+Console.Write($"[");
+for (int i = 0; i < 4; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write($"{array[i]} ");
+    if (array[i] % 2 == 0)
+    {
+        a = a + 1;
+    }
+}
+Console.WriteLine($"] -> {a}");
+
+
